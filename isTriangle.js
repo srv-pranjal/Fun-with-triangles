3917e67cf1d7562f59f5b99eb5b9d1ca00364bed
srv-pranjal <prns366@gmail.com>
2021-09-05 00:20:02 +0530

    docs: added isTriangle.js

---
+++ isTriangle.js
@@ -0,0 +1,26 @@
+const inputAngles = document.querySelectorAll(".input-angles");
+const outputMessage = document.querySelector("#output");
+const formID = document.querySelector("#form");
+
+function checkIfAnglesFormTriangle() {
+  let sum = 0;
+  inputAngles.forEach((angle) => {
+    sum += Number(angle.value);
+  });
+  if (sum === 180) return true;
+  return false;
+}
+
+function formSubmitHandler(e) {
+  e.preventDefault();
+  const isTriangle = checkIfAnglesFormTriangle();
+  if (isTriangle) {
+    outputMessage.innerText =
+      "Wohoooo!! These angles together do make a triangle";
+  } else {
+    outputMessage.innerText =
+      "Oh no!! These angles together cannot make a triangle. Why?\nHint: Sum of angles = 180";
+  }
+}
+
+formID.addEventListener("submit", formSubmitHandler);
